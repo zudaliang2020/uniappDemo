aab4e3b695d6ac489daea63f5c9068b218201e11
zudaliang2020 <1277872853@qq.com>
2024-07-30 20:05:36 +0800

    静态页面完成

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -3,9 +3,9 @@
 		<view class="head">
 			<view class="title"><text>计划航班</text></view>
 			<view class="iconBox">
-				<uni-icons class="icon" type="search" size="20" color="white"></uni-icons>
+				<uni-icons class="icon" type="search" size="20" color="#c5c5c5"></uni-icons>
 				<uni-badge class="uni-badge-left-margin" :text="22" absolute="rightTop" size="small">
-					<uni-icons class="icon" type="notification" size="20" color="white"></uni-icons>
+					<uni-icons class="icon" type="notification" size="20" color="#c5c5c5"></uni-icons>
 				</uni-badge>
 			</view>
 		</view>
@@ -19,24 +19,113 @@
 					<view class="th"><text>机位-准备室</text></view>
 					<view class="th"><text>机组</text></view>
 				</view>
-				<view class="tr">
-					<view class="td" title="flightSchedule"><text>06/20<br>08:05</text></view class="td">
-					<view class="td" title="flightNumber"><text>S9801</text></view class="td">
-					<view class="td" title="aircraftNumber"><text>B2972</text></view class="td">
-					<view class="td" title="airport"><text>ZBAA-ZGSZ</text></view class="td">
-					<view class="td" title="boardingGate"><text>221-02</text></view class="td">
-					<view class="td" title="aircrew"><text class="view">查看</text></view class="td">
+				<view class="scroll-box">
+					<view class="tr">
+						<view class="td" title="flightSchedule"><text>06/20<br>08:05</text></view class="td">
+						<view class="td" title="flightNumber"><text>S9801</text></view class="td">
+						<view class="td" title="aircraftNumber"><text>B2972</text></view class="td">
+						<view class="td" title="airport"><text>ZBAA-ZGSZ</text></view class="td">
+						<view class="td" title="boardingGate"><text class="boardingGate">221-02</text></view class="td">
+						<view class="td" title="aircrew"><text class="view" @click="open">查看</text></view class="td">
+						<view class="td favorite hidden" title="favorite"><text>收藏</text></view class="td">
+					</view>
+					<view class="tr">
+						<view class="td" title="flightSchedule"><text>06/20<br>08:05</text></view class="td">
+						<view class="td" title="flightNumber"><text>S9801</text></view class="td">
+						<view class="td" title="aircraftNumber"><text>B2972</text></view class="td">
+						<view class="td" title="airport"><text>ZBAA-ZGSZ</text></view class="td">
+						<view class="td" title="boardingGate"><text class="boardingGate">-</text></view class="td">
+						<view class="td" title="aircrew"><text class="view">查看</text></view class="td">
+						<view class="td favorite hidden" title="favorite"><text>收藏</text></view class="td">
+					</view>
+					<view class="tr">
+						<view class="td" title="flightSchedule"><text>06/20<br>08:05</text></view class="td">
+						<view class="td" title="flightNumber"><text>S9801</text></view class="td">
+						<view class="td" title="aircraftNumber"><text>B2972</text></view class="td">
+						<view class="td" title="airport"><text>ZBAA-ZGSZ</text></view class="td">
+						<view class="td" title="boardingGate"><text class="boardingGate">221-02</text></view class="td">
+						<view class="td" title="aircrew"><text class="view">查看</text></view class="td">
+						<view class="td favorite" title="favorite"><text>收藏</text></view class="td">
+					</view>
+					<view class="tr">
+						<view class="td" title="flightSchedule"><text>06/20<br>08:05</text></view class="td">
+						<view class="td" title="flightNumber"><text>S9801</text></view class="td">
+						<view class="td" title="aircraftNumber"><text>B2972</text></view class="td">
+						<view class="td" title="airport"><text>ZBAA-ZGSZ</text></view class="td">
+						<view class="td" title="boardingGate"><text class="boardingGate">221-02</text></view class="td">
+						<view class="td" title="aircrew"><text class="view">查看</text></view class="td">
+						<view class="td favorite active" title="favorite"><text>取消收藏</text></view class="td">
+					</view>
 				</view>
-				<view class="tr">
-					<view class="td" title="flightSchedule"><text>06/20<br>08:05</text></view class="td">
-					<view class="td" title="flightNumber"><text>S9801</text></view class="td">
-					<view class="td" title="aircraftNumber"><text>B2972</text></view class="td">
-					<view class="td" title="airport"><text>ZBAA-ZGSZ</text></view class="td">
-					<view class="td" title="boardingGate"><text>221-02</text></view class="td">
-					<view class="td" title="aircrew"><text class="view">查看</text></view class="td">
-				</view>
+
 			</view>
 		</view>
+		<uni-popup ref="showDetails" type="center" :animation="false" :mask-click="false">
+			<view class="datails-box">
+				<view class="close" @click="close"><uni-icons type="close" size="16" color="#c5c5c5"></uni-icons></view>
+				<view class="table-box">
+					<text>机组人员</text>
+					<view class="table" border stripe emptyText="暂无更多数据">
+						<!-- 表头行 -->
+						<view class="tr">
+							<view class="th"><text></text></view>
+							<view class="th"><text>张三</text></view>
+							<view class="th"><text>李四</text></view>
+							<view class="th"><text>xxx</text></view>
+						</view>
+						<!-- 表格数据行 -->
+						<view class="tr">
+							<view class="td"><text>机上岗位</text></view>
+							<view class="td"><text>责任机长</text></view>
+							<view class="td"><text>副驾</text></view>
+							<view class="td"><text>跟班学员</text></view>
+						</view>
+						<view class="tr">
+							<view class="td"><text>技术等级</text></view>
+							<view class="td"><text>TB</text></view>
+							<view class="td"><text>TB</text></view>
+							<view class="td"><text>F3</text></view>
+						</view>
+						<view class="tr">
+							<view class="td"><text>左座飞行经历时间</text></view>
+							<view class="td"><text>2345.5h</text></view>
+							<view class="td"><text>1345.5h</text></view>
+							<view class="td"><text>345.5h</text></view>
+						</view>
+						<view class="tr">
+							<view class="td"><text>右座飞行经历时间</text></view>
+							<view class="td"><text>xx</text></view>
+							<view class="td"><text>xx</text></view>
+							<view class="td"><text>xx</text></view>
+						</view>
+						<view class="tr">
+							<view class="td"><text>责任机长飞行经历时间</text></view>
+							<view class="td"><text>xx</text></view>
+							<view class="td"><text>xx</text></view>
+							<view class="td"><text>xx</text></view>
+						</view>
+						<view class="tr">
+							<view class="td"><text>左座次数</text></view>
+							<view class="td"><text>423</text></view>
+							<view class="td"><text>xx</text></view>
+							<view class="td"><text>xx</text></view>
+						</view>
+						<view class="tr">
+							<view class="td"><text>右座次数</text></view>
+							<view class="td"><text>543</text></view>
+							<view class="td"><text>xx</text></view>
+							<view class="td"><text>xx</text></view>
+						</view>
+						<view class="tr">
+							<view class="td"><text>着陆提纵次数</text></view>
+							<view class="td"><text>293</text></view>
+							<view class="td"><text>xx</text></view>
+							<view class="td"><text>xx</text></view>
+						</view>
+					</view>
+				</view>
+			</view>
+		</uni-popup>
 		<view class="navigation">
 			<view class="box now">
 				<view class="icon"></view>
@@ -56,34 +145,48 @@
 			</view>
 		</view>
 	</view>
-	
+
 </template>
 
-<script setup>
-
+<script>
+	export default {
+		methods: {
+			open() {
+				// 通过组件定义的ref调用uni-popup方法 ,如果传入参数 ，type 属性将失效 ，仅支持 ['top','left','bottom','right','center']
+				this.$refs.showDetails.open('center')
+			},
+			close() {
+				this.$refs.showDetails.close()
+			}
+		}
+	}
 </script>
 
 <style lang="scss">
-	.layout{
+	.layout {
 		height: 100vh;
 		background: #2C3F60;
 		color: white;
 	}
+
 	.head {
 		height: 50px;
 		display: flex;
-		.title{
+
+		.title {
 			width: 80vw;
 			display: flex;
 			justify-content: center;
 			align-items: center;
 		}
-		.iconBox{
+
+		.iconBox {
 			width: 20vw;
 			display: flex;
 			justify-content: center;
 			align-items: center;
-			.icon{
+
+			.icon {
 				padding: 2px;
 				border: 1px solid #555555;
 				border-radius: 50%;
@@ -93,35 +196,118 @@
 	}
 
 	.content {
-		height: calc(100% - 110px);
+
 		.table {
 			font-size: 12px;
 			color: white;
+
+			.scroll-box {
+				width: 100vw;
+				height: calc(100vh - 146px);
+				overflow-y: scroll;
+				/* 允许 Y 轴滚动 */
+				overflow-x: hidden;
+				/* 隐藏 X 轴的滚动条 */
+				scrollbar-width: none;
+				/* Firefox */
+				-ms-overflow-style: none;
+				/* Internet Explorer and Edge */
+			}
+
+			.scroll-box::-webkit-scrollbar {
+				display: none;
+				/* Safari and Chrome */
+			}
+
 			.tr {
 				display: flex;
 				flex-direction: row;
 				justify-content: center;
 				align-items: center;
+
 				.th {
 					background: #25324F;
 					height: 36px;
-					flex: auto;
+					flex: 1;
 					display: flex;
 					justify-content: center;
 					align-items: center;
+
 				}
+
 				.td {
-					
+
 					height: 40px;
-					border-bottom:1px solid #445677;
-					flex: auto;
+					border-bottom: 1px solid #445677;
+					flex: 1;
 					display: flex;
 					justify-content: center;
 					align-items: center;
-					.view{
+
+					.view {
 						color: #409EFF;
 					}
+
+					.boardingGate {
+						text-align: center;
+					}
+
 				}
+
+				.favorite {
+					width: 48px;
+					text-align: center;
+					background: #409EFF;
+				}
+
+				.active {
+					background: #F59A23;
+				}
+
+				.hidden {
+					display: none;
+				}
+			}
+		}
+	}
+
+	.datails-box {
+		width: 100vw;
+		padding: 5vh 0;
+		background: #25324F;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		.close {
+			width: 16px;
+			height: 16px;
+			position: absolute;
+			top: 2vh;
+			right: 4vw;
+		}
+
+		.table-box {
+			width: 90vw;
+			background: #2C3F60;
+			border-radius: 1%;
+			padding: 8px;
+			.table {
+				background: #2C3F60;
+				border-collapse: collapse;
+				.tr {
+					display: flex;
+					flex-direction: row;
+					justify-content: center;
+
+					.th,
+					.td {
+						border: 1px solid #555555;
+						flex: 1 ;
+						padding: 8px;
+					}
+				}
+
 			}
 		}
 	}
@@ -133,25 +319,29 @@
 		display: flex;
 		justify-content: center;
 		align-items: center;
-		.box{
+
+		.box {
 			flex: auto;
 			display: flex;
 			flex-direction: column;
 			justify-content: center;
 			align-items: center;
-			.icon{
+
+			.icon {
 				width: 24px;
 				height: 24px;
 				border-radius: 50%;
 				background: #AAAAAA;
 			}
 		}
-		.now{
+
+		.now {
 			color: white;
-			.icon{
+
+			.icon {
 				background-color: white;
 			}
 		}
-		
+
 	}
 </style>
